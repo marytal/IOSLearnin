051e12bb99d4df2d75da79042df2c841a1f5d096
Mary Briskin <mary.briskin@futureadvisor.com>
2016-03-01 10:38:03 -0800

    Initial Commit

---
+++ takeTwo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  takeTwo
+//
+//  Created by Mary Briskin on 3/1/16.
+//  Copyright © 2016 Mary Briskin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
